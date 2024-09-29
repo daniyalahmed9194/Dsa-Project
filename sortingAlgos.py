53c275271f1557af3641c772d563f4e1691d0898
Ali <malihumayon@gmail.com>
2024-09-29 14:49:17 +0500

    Merge and Quick Added

--- sortingAlgos.py
+++ sortingAlgos.py
@@ -36,6 +36,56 @@ def selectionSort (array,column):
         array[i], array[min] = array[min], array[i]
     return array
 
+def sortQuick (array, column):
+    n = len(array)
+    if n <= 1:
+        return array
+    else:
+        pivot = array[n // 2]
+        rightArray = [x for x in array if x[column] > pivot[column]]
+        leftArray = [x for x in array if x[column] < pivot[column]]
+        middleArray = [x for x in array if x[column] == pivot[column]]
+
+        return sortQuick (leftArray,column) + middleArray + sortQuick(rightArray,column)
+
+def mergeSort(array, column):
+    if len(array) > 1:
+        mid = len(array) // 2  
+        left_half = array[:mid]  
+        right_half = array[mid:]
+
+       
+        mergeSort(left_half, column)
+        mergeSort(right_half, column)
+
+        i = j = k = 0
+
+    
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i][column] < right_half[j][column]:
+                array[k] = left_half[i]
+                i += 1
+            else:
+                array[k] = right_half[j]
+                j += 1
+            k += 1
+
+       
+        while i < len(left_half):
+            array[k] = left_half[i]
+            i += 1
+            k += 1
+
+       
+        while j < len(right_half):
+            array[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return array
+
+
+
 
 array = [
     [3, 'Alice', 85],
@@ -44,5 +94,10 @@ array = [
     [4, 'David', 95],
     [5, 'Eve', 80]
 ]
+
+sortedArray = mergeSort(array, 1)
+
+for row in sortedArray:
+    print(row)
 
 
